896e7b11642356660f2c4c2f267967ff2249c845
Eric Selin <eric.selin@gmail.com>
2021-03-11 21:40:27 +0100

    fix: Change config file editing to run via sh

--- scripts/edit-config-file.sh
+++ scripts/edit-config-file.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Select a config file in the dotfiles repo for editing in vim.
-# Will make it way easier to edit, with the added bonus of
-# promoting committing config files to the repo so they can
-# be easily edited!
-
 # This is the directory where the .git folder lives
 DOTS_GIT=$HOME/.dot.git
 
@@ -14,6 +9,5 @@ dotfile_paths=$(/usr/bin/git --git-dir=$DOTS_GIT --work-tree=$HOME ls-files)
 # -no-custom means don't allow custom input
 chosen="$(echo -e "$dotfile_paths" | dmenu -i -l 10 -p 'Edit config file:')"
 
-# Run in vim or message user
-[ -f "$chosen" ] && alacritty -e vim $chosen || echo "No such file"
+[ -f "$chosen" ] && alacritty -e /bin/sh -lc "vim $chosen" || dunstify "No such file"
 
