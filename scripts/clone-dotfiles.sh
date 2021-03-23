fa209e452a8e1ee53ecccb3e259c94aa35c036bc
Eric Selin <eric.selin@gmail.com>
2021-03-23 18:45:01 +0100

    feat: Add script for cloning dotfiles into home dir

---
+++ scripts/clone-dotfiles.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+git clone --bare https://bitbucket.org/durdn/cfg.git $HOME/.dot.git
+function config {
+   /usr/bin/git --git-dir=$HOME/.dot.git/ --work-tree=$HOME $@
+}
+config checkout
+if [ $? = 0 ]; then
+  echo "Checked out config.";
+  else
+    echo "Backing up pre-existing dot files.";
+    mkdir -p .dotfiles-backup
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
+fi;
+config checkout
+config config status.showUntrackedFiles no
