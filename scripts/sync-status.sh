5f0185c18ec682e727d367d7ec08ef09f7a81637
Eric Selin <eric.selin@gmail.com>
2021-05-26 13:12:13 +0200

    wip: Create warning in sync status when service failed

--- scripts/sync-status.sh
+++ scripts/sync-status.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
+
+# output all info
 systemctl --user show mbsync outlook-calendar-sync vdirsyncer -p Id,ExecMainStatus,ActiveState,ExecMainStartTimestamp
+
+services="mbsync outlook-calendar-sync vdirsyncer"
+
+# see if anything has failed
+_=$(systemctl --user show $services -p ActiveState | grep -F 'ActiveState=failed')
+# check exit code here
+if [ $? -eq 0 ] ; then
+  echo ''
+  echo 'FAILED COMMANDS!'
+fi
