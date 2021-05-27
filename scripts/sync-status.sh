2802967d3a36a508f832d2076398e522fd8f55e9
Eric Selin <eric.selin@gmail.com>
2021-05-27 17:00:16 +0200

    feat: Add sync status to waybar

--- scripts/sync-status.sh
+++ scripts/sync-status.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
 
-# output all info
-systemctl --user show mbsync outlook-calendar-sync vdirsyncer -p Id,ExecMainStatus,ActiveState,ExecMainStartTimestamp
-
 services="mbsync outlook-calendar-sync vdirsyncer"
 
+output() {
+  local status=$(systemctl --user show $services -p Id,ExecMainStatus,ActiveState,ExecMainStartTimestamp)
+  # first replace newline with tab to get everything on one line so sed is happy
+  # then substitute tab with literal '\n'
+  local tooltip=$(echo -n "$status" | tr '\n' '\t' | sed 's/\t/\\n/g')
+  echo "{\"text\": \"$1\", \"tooltip\": \"$tooltip\", \"class\": \"$2\"}"
+  exit
+}
+# output all info
+
 # see if anything has failed
-_=$(systemctl --user show $services -p ActiveState | grep -F 'ActiveState=failed')
-# check exit code here
+systemctl --user is-failed $services --quiet
+# exit code is 0 if something has failed
 if [ $? -eq 0 ] ; then
-  echo ''
-  echo 'FAILED COMMANDS!'
+  output 'sync failure' error
 fi
+
+# see if sync more than 20 min age
+now=$(date +%s)
+systemctl --user show $services -P ExecMainStartTimestamp | grep -v '^$' | while read time;
+do
+  epoch=$(date -d "$time" +%s)
+  if (( $now - $epoch > 20 * 60 )) ; then
+    output 'sync delay' warning
+  fi
+done
