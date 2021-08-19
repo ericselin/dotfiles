1ae4e63c2b51380341b1ad1c91567ee2b08aa11f
Eric Selin <eric.selin@gmail.com>
2021-08-19 16:06:53 +0200

    Split cloning dotfiles and restoring system into separate scripts

---
+++ scripts/clone-dotfiles.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Clone dotfiles repository as a bare repo in the home directory
+
+# Run command on dotfiles repository
+_dot () {
+  git --git-dir=$HOME/.dot.git/ --work-tree=$HOME $@
+}
+
+echo 'Starting system restore'
+
+echo 'Installing git...'
+sudo pacman -Sy --needed git
+
+if _dot status; then
+  echo 'Found dotfiles repository, pulling...'
+  _dot pull
+else
+  echo 'Dotfiles repository not found, cloning...'
+  git clone --recurse-submodules --bare https://github.com/ericselin/dotfiles.git $HOME/.dot.git
+  _dot checkout
+  if [ $? = 0 ]; then
+    echo "Checked out config";
+  else
+    echo "Backing up pre-existing dot files...";
+    mkdir -p .dotfiles-backup
+    _dot checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
+    _dot checkout
+  fi;
+  _dot config status.showUntrackedFiles no
+fi;
--- scripts/restore-system.sh
+++ scripts/restore-system.sh
@@ -4,38 +4,6 @@
 # This script should be indepodent, so it can be run
 # on an existing system as well
 
-# Run command on dotfiles repository
-_dot () {
-  git --git-dir=$HOME/.dot.git/ --work-tree=$HOME $@
-}
-
-_log () {
-  echo $@
-}
-
-_log 'Starting system restore'
-
-_log 'Installing git...'
-sudo pacman -Sy --needed git
-
-if _dot status; then
-  _log 'Found dotfiles repository, pulling...'
-  _dot pull
-else
-  _log 'Dotfiles repository not found, cloning...'
-  git clone --recurse-submodules --bare https://github.com/ericselin/dotfiles.git $HOME/.dot.git
-  _dot checkout
-  if [ $? = 0 ]; then
-    echo "Checked out config";
-  else
-    echo "Backing up pre-existing dot files...";
-    mkdir -p .dotfiles-backup
-    _dot checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
-    _dot checkout
-  fi;
-  _dot config status.showUntrackedFiles no
-fi;
-
 _log 'Installing packages...'
 sudo pacman -S --needed - < ~/.packages/explicit
 
