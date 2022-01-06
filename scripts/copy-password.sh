457ccfda57778e8cb14735cafe779ac50041e135
Eric Selin <eric.selin@gmail.com>
2022-01-06 21:35:14 +0100

    sway: Terminal menu for passwords and config files

--- scripts/copy-password.sh
+++ scripts/copy-password.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-input=$(gopass list -f | bemenu -p "gopass")
+input=$(gopass list -f | fzf --layout=reverse --info=hidden)
+[ -z "$input" ] && exit
+wl-copy --clear
 printf '%s' "$(gopass show -o "$input")" | wl-copy
 sleep 10
 wl-copy --clear
