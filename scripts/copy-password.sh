8ab8d063405d570c170655fe7107fcb8ed6b58b3
Eric Selin <eric.selin@gmail.com>
2022-01-10 12:57:22 +0100

    Use wl-copy with paste once for copying passwords

--- scripts/copy-password.sh
+++ scripts/copy-password.sh
@@ -2,7 +2,6 @@
 
 input=$(gopass list -f | fzf --layout=reverse --info=hidden)
 [ -z "$input" ] && exit
+echo 'Decrypting password...'
 wl-copy --clear
-printf '%s' "$(gopass show -o "$input")" | wl-copy
-sleep 10
-wl-copy --clear
+printf '%s' "$(gopass show -o "$input")" | wl-copy -o
