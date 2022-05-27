0a3d2e8e6a08fbddb3594f8d67122ea23358ca63
Eric Selin <eric.selin@gmail.com>
2022-05-27 09:48:17 +0200

    Fix pomodoro starting by changing killall target to full script name

--- scripts/pomodoro.sh
+++ scripts/pomodoro.sh
@@ -3,9 +3,9 @@
 if [ "$1" == 'stop' ];
 then
   echo 'Stopping pomodoro'
-  killall -SIGUSR2 pomodoro-service
+  killall -SIGUSR2 pomodoro-service.sh
 else
   echo 'Starting pomodoro'
-  killall -SIGUSR1 pomodoro-service
+  killall -SIGUSR1 pomodoro-service.sh
 fi
 
