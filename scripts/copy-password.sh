218ae0b801b42a2dafc586762a5d3290be2fd08e
Eric Selin <eric.selin@gmail.com>
2023-01-03 19:54:09 +0100

    Create command in pw prompt to create new pw

--- scripts/copy-password.sh
+++ scripts/copy-password.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-input=$(gopass list -f | fzf --layout=reverse --info=hidden)
+newcmd='[Create new password]'
+input=$((echo -e $newcmd'\n'; gopass list -f) | fzf --layout=reverse --info=hidden)
+
 [ -z "$input" ] && exit
+
+[ "$input" == "$newcmd" ] && { gopass create; exit; }
+
 echo 'Decrypting password...'
 wl-copy --clear
 printf '%s' "$(gopass show -o "$input")" | wl-copy -o
