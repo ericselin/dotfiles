2df4763446890caab5c8a9863512a008c73e92c8
Eric Selin <eric.selin@gmail.com>
2023-03-26 21:10:12 +0200

    menu: Wait for key press if non-zero exit code

--- scripts/fzmenu.sh
+++ scripts/fzmenu.sh
@@ -7,3 +7,8 @@ cmd="$(find . -type f -executable -printf '%P\n' | fzf --layout=reverse --info=h
 [ -z "$cmd" ] && exit
 
 "./$cmd"
+
+if [ $? -gt 0 ]; then
+echo
+read -p 'Press any key to exit...'
+fi
