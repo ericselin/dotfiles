1ff3fd281dfe0edc37ece6f12113776fafc62322
Eric Selin <eric.selin@gmail.com>
2024-02-09 23:01:59 +0100

    feat/fzf: Enable multi-select in dot add fzf
    This commit just adds the --multi argument to fzf to enable multi-select.
    
    Also now not relying on the `dot` alias in the add script, but rather
    using GIT_OPTS everywhere. And fixed a bug with unquoted strings
    (multiple selections separated by new line).

--- scripts/dotfiles-add.sh
+++ scripts/dotfiles-add.sh
@@ -1,29 +1,37 @@
 # Add selected file or directery to the dotfiles repo
 
-GIT_OPTS="--git-dir=$HOME/.dot.git/ --work-tree=$HOME"
-
 function dotadd() {
+  local GIT_OPTS="--git-dir=$HOME/.dot.git/ --work-tree=$HOME"
   # unstaged changes
-  local unstaged=$(dot diff --name-only)
+  local unstaged=$(git $GIT_OPTS diff --name-only)
   # get untracked files
-  local untracked=$(dot ls-files --others --directory --exclude-standard --empty-directory :/)
+  local untracked=$(git $GIT_OPTS ls-files --others --directory --exclude-standard --empty-directory :/)
   # get directory where the file lives
   # the preview command shows dir contents if its a dir, otherwise file contents with bat
-  local path=$(printf "$unstaged\n\n$untracked" | fzf --preview="[ -d {} ] && ls -la {} || ([ -z {} ] && echo 'Unstaged below, untracked above' || (! git $GIT_OPTS diff --exit-code --color -- {} || bat --color=always {}))")
+  local path=$(printf "$unstaged\n\n$untracked" | \
+    fzf --multi --preview="[ -d {} ] && ls -la {} \
+      || ([ -z {} ] && echo 'Unstaged below, untracked above' \
+      || ( \
+        ! git $GIT_OPTS diff --exit-code --color -- {} \
+        || bat --color=always {}) \
+      )"
+    )
 
   # if selected was a directory, get file names from there
-  if [ $path ] && [ -d $path ]; then
+  if [ "$path" ] && [ -d "$path" ]; then
     # get the path of the file
-    path=$(dot ls-files --others --exclude-standard $path | fzf --preview='bat {}')
+    # preview can be bat, because modified files are never listed as dirs
+    path=$(git $GIT_OPTS ls-files --others --exclude-standard $path \
+      | fzf --multi --preview='bat {}')
   fi
 
   # exit if nothing selected
-  if [ -z $path ]; then
+  if [ -z "$path" ]; then
     echo 'Nothing selected, nothing added.'
     return
   fi
 
   # add it!
   echo "Adding $path"
-  dot add $path
+  git $GIT_OPTS add $path
 }
